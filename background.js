fd6dae6333dce30c851efe217d80f80e7cd7df49
Siddharth <siddharth.vjay333@gmail.com>
2021-02-14 16:36:05 -0500

    wrap-up UI and basic storage functionality

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+	chrome.storage.sync.set({ color });
+	console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+chrome.runtime.onMessage.addListener((data) => {
+	console.log('hello');
+	if (data.type === 'notification') {
+		chrome.notifications.create('id', data.options, function(id) {});
+	}
 });
--- popup.js
+++ popup.js
@@ -1,9 +1,37 @@
-let visible = false;
-console.log(visible);
-function toggleTitleVisibility() {
-    console.log("hello");
-    visible = !visible;
-    document.getElementById("title-1").style.visibility= visible ? "visible" : "hidden";
+let current_page = 'signup';
+console.log(current_page);
+var opt = {
+	type: 'basic',
+	title: 'Primary Title',
+	message: 'Primary message to display',
+	iconUrl: 'url_to_small_icon'
+};
+
+function togglePage() {
+	if (current_page == 'signup') {
+		console.log('in signup page');
+		document.getElementById('main').style.display = 'block';
+		document.getElementById('signup').style.display = 'none';
+		current_page = 'main';
+	} else if (current_page == 'main') {
+		document.getElementById('main').style.display = 'none';
+		document.getElementById('signup').style.display = 'block';
+		current_page = 'signup';
+	}
 }
 
-document.getElementById("toggle-button").addEventListener('click', toggleTitleVisibility);
+function confirmButtonOnClick() {}
+function sendThoughts() {}
+function verifySetup() {
+	chrome.storage.sync.get('partner-username', function(obj) {
+		if (obj['partner-username'] && obj['username']) {
+			console.log('usernames set');
+		} else {
+			console.log('object doesnt exist');
+		}
+	});
+}
+
+verifySetup();
+document.getElementById('submit-button').addEventListener('click', togglePage);
+document.getElementById('thinking-button').addEventListener('click', togglePage);
